177946af5bd6415ecc38967681fbfdf347dc699c
copymachine <copymach@naver.com>
2022-01-10 09:49:29 +0900

    컨트롤러 수정

--- WebContent/WEB-INF/deleteForm.jsp
+++ WebContent/WEB-INF/deleteForm.jsp
@@ -1,12 +1,52 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<%@ page import="com.javaex.vo.GuestbookVo"%>
+
+<%
+
+GuestbookVo guestbookVo = (GuestbookVo)request.getAttribute("gbVo");
+
+//addList 에서 받아온 id를 숫자로 바꿔준다
+int ContentId = Integer.parseInt(request.getParameter("id"));
+
+int id = guestbookVo.getNo();
+String password = guestbookVo.getPassword();
+%>
+
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title> 방명록 글 삭제폼 model2</title>
 </head>
 <body>
+
+	<h2>글 삭제하려면 비밀번호를 입력하고 삭제버튼을 누르세요.</h2>
+	<br>
+
+	<form action="/guestbook2/gbc" method="get">
+		<table>
+			<tr>
+				<td>방명록 글ID (type=hidden 처리예정)</td>
+				<td><input type="text" name="no" value="<%=id%>" readonly>
+				<input type="hidden" name="action" value="delete">
+				</td>
+			</tr>
+
+			<tr>
+				<td>비밀번호</td>
+				<td><input type="password" name="password"
+					value="<%=password%>">
+					<button type="submit">삭제</button></td>
+			</tr>
+		</table>
+	</form>
+	<br>
+	<br>
+	<a href="/WEB-INF/addList.jsp"> 목록으로 돌아가기 (상대경로)</a>
+	<br>
+
 
 </body>
 </html>
--- src/com/javaex/controller/GuestbookController.java
+++ src/com/javaex/controller/GuestbookController.java
@@ -45,8 +45,8 @@ public class GuestbookController extends HttpServlet {
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/addList.jsp");
 			rd.forward(request, response);
 
-//			게스트북 에러 확인
-			System.out.println(guestbookList);
+//			게스트북 에러 확인 에러안남
+//			System.out.println(guestbookList);
 
 //		방명록글 써넣기
 		} else if ("write".equals(act)) {
@@ -56,7 +56,7 @@ public class GuestbookController extends HttpServlet {
 			String name = request.getParameter("name");
 			String password = request.getParameter("password");
 			String content = request.getParameter("content");
-			//String regDate = request.getParameter("regDate");
+//			String regDate = request.getParameter("regDate");
 			
 //			vo로 만든다
 			GuestbookVo guestbookVo = new GuestbookVo(name, password, content);
@@ -67,19 +67,42 @@ public class GuestbookController extends HttpServlet {
 
 			guestbookDao.ContentInsert(guestbookVo);
 
-//			포워드 -> 서버 내 사이클 돌때
-			response.sendRedirect("/phonebook2/pbc?action=addList");
+//			리다이렉트
+			response.sendRedirect("/guestbook2/gbc?action=addList");
 			
 			
-//		삭제 폼	
+//		삭제 폼 addList에서 글id 받아서 포워딩해줘야함	
 		} else if ("deleteForm".equals(act)) {
 			System.out.println("action = deleteForm");
-		
+			
+//			id 형변환
+			int no = Integer.parseInt(request.getParameter("no"));
+			
+//			숫자로 변경한 no로 대상 방명록글 식별
+			GuestbookVo guestbookVo = new GuestbookDao().getNo(no); 
+			
+//			Action으로 넘어온 값을 변경시킨후 JSP 페이지로 넘겨주기
+			request.setAttribute("gbVo", guestbookVo);
+			
+//			포워드 
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/deleteForm.jsp");
+			rd.forward(request, response);
+			
 			
 //		삭제	
 		} else if ("delete".equals(act)) {
 			System.out.println("action = delete");
 			
+			GuestbookDao guestbookDao = new GuestbookDao();
+
+			//addList 에서 받아온 id를 숫자로 바꿔준다
+			int ContentId = Integer.parseInt(request.getParameter("id"));
+
+			GuestbookVo guestbookVo = guestbookDao.getNo(ContentId);
+
+			int id = guestbookVo.getNo();
+			String password = guestbookVo.getPassword();
+				
 			
 		} else {
 			System.out.println("파라미터 값 없음");
